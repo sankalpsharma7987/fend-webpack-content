2545055722039da291b7ab04b059aecebbcb1905
sankalpsharma7987@gmail.com <sankalpsharma7987@gmail.com>
2021-02-07 00:08:03 -0500

    New Loader Branch with File Loader

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,5 @@
 import {handleSubmit} from './js/formHandler';
 import {nameChecker} from './js/nameChecker';
+import {img} from '../images/my_image.jpg';
 
 alert("I EXIST!");
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,27 @@ const webpack = require('webpack')
 
 module.exports = {
     entry: './src/client/index.js',
+    output : {
+        path:path.resolve(__dirname,"build/js"),
+        filename:'bundle.js'
+    },
     module :{
         rules:[
-            {
+            {   
                 test:'/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                exclude: /node_modules/,
+                loader: 'file-loader', //The file-loader resolves import/require() on a file into a url and emits the file into the output directory
+                options: {
+                    outputPath: "../images",
+                    name: '[name]_new_1.[ext]'
+                },
+              }
         ]
     }
 }
+
